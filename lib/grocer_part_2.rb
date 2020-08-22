e4ec9ecb90ad2fc60a549f852f88e5d4df007bcc
Will Lytle <wlytle89@gmail.com>
2020-08-22 19:09:57 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,9 +1,38 @@
 require_relative './part_1_solution.rb'
+require 'pry'
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  coupon_item = {}
+  coupons.each do |coupon|
+    cart.each do |grocery|
+      if grocery[:item] === coupon[:item]
+        
+        #make cupon price per item
+        if coupon[:num] > 1 
+          new_cost = coupon[:cost] = coupon[:cost]/coupon[:num]
+        end
+        
+        #Set up new discounted item for cart
+        coupon_item = {}
+        coupon_item[:item] = "#{grocery[:item]} W/COUPON"
+        coupon_item[:price] = new_cost
+        coupon_item[:clearance] = grocery[:clearance]
+        
+        if coupon[:num] >= grocery[:count]
+          coupon_item[:count] = grocery[:count]
+          grocery[:count] = 0
+        else
+          #binding.pry
+          coupon_item[:count] = coupon[:num]
+          grocery[:count] = grocery[:count] - coupon_item[:count]
+          
+          #binding.pry
+        end
+        cart << coupon_item
+      end
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)
