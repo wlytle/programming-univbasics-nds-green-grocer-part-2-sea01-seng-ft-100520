92e9d22560db1724d338077280eb15374d1f8195
Will Lytle <wlytle89@gmail.com>
2020-08-22 20:07:48 +0000

    Done.

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,25 +1,55 @@
 require_relative './part_1_solution.rb'
+require 'pry'
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  coupons.each do |coupon|
+    cart.each do |grocery|
+      if grocery[:item] === coupon[:item] && grocery[:count] >= coupon[:num]
+        
+        #make cupon price per item
+          new_cost = coupon[:cost]/coupon[:num]
+        
+        #Set up new discounted item for cart
+        coupon_item = {}
+        coupon_item[:item] = "#{grocery[:item]} W/COUPON"
+        coupon_item[:price] = new_cost
+        coupon_item[:clearance] = grocery[:clearance]
+        coupon_item[:count] = coupon[:num]
+        grocery[:count] = grocery[:count] - coupon_item[:count]
+        cart << coupon_item
+      end
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  cart.each do |grocery|
+    if grocery[:clearance]
+      grocery[:price] *= 0.8
+      grocery[:price].round(2)
+    end
+  end
+  cart
 end
 
 def checkout(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
+  total = 0
+  #binding.pry
+  cart = consolidate_cart(cart)
+  #binding.pry
+  cart = apply_coupons(cart, coupons)
+  #binding.pry
+  cart = apply_clearance(cart)
+  #binding.pry
+  
+  cart.each do |grocery|
+    total += grocery[:price] * grocery[:count]
+  end
+  
+  if total > 100
+    total *= 0.9
+  end
+  #binding.pry
+  total.round(2)
 end
